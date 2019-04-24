7f9ccba2730d459c071c5f19135918c1134bbe18
prochini <a77471@gmail.com>
2019-04-24 15:49:03 +0800

    week2

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,6 +1,4 @@
-/* eslint linebreak-style: ["error", "windows"] */
-function capitalize(str) { // \r\n
-  // do stuff \r\n
+function capitalize(str) {
   return str.charAt(0).toUpperCase() + str.substring(1);
-} // \r\n
-console.log(capitalize('nick')); // \r\n
+}
+console.log(capitalize('nick'));
--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,12 +1,12 @@
 /* eslint linebreak-style: ["error", "windows"] */
-let total = '';
+
 function reverse(str) { // \r\n
   // do stuff \r\n
-  const Num = str.length - 1;
-  for (let i = Num; i >= 0; i -= 1) {
+  let total = '';
+  const num = str.length - 1;
+  for (let i = num; i >= 0; i -= 1) {
     total += str[i];
   }
   return total;
 }// \r\n
-
 console.log(reverse('1,2,3,2,2'));// \r\n
