80e13620840721fa099c70780808612ca5f9ab01
prochini <a77471@gmail.com>
2019-06-08 08:52:02 +0800

    RWD justice

--- homeworks/week8/hw2/index.js
+++ homeworks/week8/hw2/index.js
@@ -1,49 +1,49 @@
-const request = new XMLHttpRequest();
-const container = document.querySelector('.wrapper');
-request.addEventListener('load', () => {
-  if (request.status >= 200 && request.status < 400) {
-    const response = request.responseText;
-    const json = JSON.parse(response);
-    for (let i = 0; i < json.length; i += 1) {
-      const div = document.createElement('div');
-      div.innerHTML = `
-        <img class="avatar"src="https://i.imgur.com/y6bVt3Y.png" alt="avatar" width="50vmin" height="50vmin">
-        <article>
-            <h1>${json[i].id}</h1>
-            <p>${json[i].content}</p>
-        </article>
-        `;
-      div.classList.add('message');
-      container.appendChild(div);
+function getComment() {
+  const container = document.querySelector('.replyComment');
+  const request = new XMLHttpRequest();
+  request.addEventListener('load', () => {
+    if (request.status >= 200 && request.status < 400) {
+      container.innerHTML = '';
+      const response = request.responseText;
+      const json = JSON.parse(response);
+      for (let i = 0; i < json.length; i += 1) {
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <article>
+                <h1>${json[i].id}</h1>
+                <p>${json[i].content}</p>
+            </article>
+            `;
+        div.classList.add('message');
+        container.appendChild(div);
+      }
+    } else {
+      alert(request.status);
     }
-  } else {
-    console.log(request.status);
-  }
-});
-request.onerror = () => {
-  console.log('error');
-};
-request.open('GET', 'https://lidemy-book-store.herokuapp.com/posts?_limit=20&_sort=id&_order=desc', true);
-
-request.send(null);
+  });
+  request.open('GET', 'https://lidemy-book-store.herokuapp.com/posts?_limit=20&_sort=id&_order=desc', true);
+  request.send(null);
+  request.onerror = () => {
+    alert('error?');
+  };
+}
+getComment();
 
 // 監聽 輸入框
 let input = '';
 document.querySelector('#content').addEventListener('input', (e) => {
   input = `content=${e.srcElement.value}`;
-  console.log(input);
 });
 
-document.querySelector('.send').addEventListener('click', () => {
+const addComment = function () {
+  document.querySelector('#content').value = '';
   const http = new XMLHttpRequest();
   const url = 'https://lidemy-book-store.herokuapp.com/posts?';
   const params = input;
   http.open('POST', url, true);
-  // Send the proper header information along with the request
   http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-  http.onreadystatechange = () => { // Call a function when the state changes.
-  };
   http.send(params);
-  alert('已送出!');
-  window.location.replace(location.href);
-});
+};
+
+document.querySelector('.send').addEventListener('click', addComment, false);
+document.querySelector('.send').addEventListener('click', () => { getComment(addComment); });
--- homeworks/week8/hw3/index.js
+++ homeworks/week8/hw3/index.js
@@ -1,36 +1,53 @@
-const request = new XMLHttpRequest();
-const container = document.querySelector('.wrapper');
-request.addEventListener('load', () => {
-  if (request.status >= 200 && request.status < 400) {
-    const response = request.responseText;
-    const json = JSON.parse(response);
-    const stream = [];
-    json.streams.forEach((e) => { stream.push(e); });
-
-    for (let i = 0; i < stream.length; i += 1) {
-      const div = document.createElement('div');
-      div.classList.add('video');
-      div.innerHTML = `            
-      <div class="box" style="cursor:pointer;"onclick="location.href='${stream[i].channel.url}';">
-      <img class="preview" src="${stream[i].preview.large}" alt="">
-      <div class="info">
-          <img class="avatar" src="${stream[i].channel.logo}" alt="">
-          <div class="title">
-          <h1 class="display_name">${stream[i].channel.status}</h1>
-          <h2 class="name">${stream[i].channel.display_name}</h2>
-          </div>
-      </div>
-      </div>
-        `;
-      container.appendChild(div);
+function loadData(origin, offset) {
+  const request = new XMLHttpRequest();
+  const container = document.querySelector(origin);
+  const url = `https://api.twitch.tv/kraken/streams/?client_id=ry76htktupl8mlicd29yf9xhrc2opo&game=League%20of%20Legends&limit=20${offset}`;
+  request.addEventListener('load', () => {
+    container.classList.add('wrapper');
+    if (request.status >= 200 && request.status < 400) {
+      const response = request.responseText;
+      const json = JSON.parse(response);
+      const stream = [];
+      json.streams.forEach((e) => { stream.push(e); });
+      for (let i = 0; i < stream.length; i += 1) {
+        const div = document.createElement('div');
+        div.classList.add('box');
+        div.innerHTML = `            
+        <div class="box" style="cursor:pointer;"onclick="location.href='${stream[i].channel.url}';">
+        <img class="preview" src="${stream[i].preview.large}" alt="">
+        <div class="info">
+            <img class="avatar" src="${stream[i].channel.logo}" alt="">
+            <div class="title">
+            <h1 class="display_name">${stream[i].channel.status}</h1>
+            <h2 class="name">${stream[i].channel.display_name}</h2>
+            </div>
+        </div>
+        </div>
+          `;
+        container.appendChild(div);
+      }
+      const updateButton = document.createElement('button');
+      updateButton.classList.add('btn');
+      updateButton.classList.add('updateButton');
+      updateButton.textContent = 'show more';
+      container.appendChild(updateButton);
+    } else {
+      console.log(request.status);
     }
-  } else {
-    console.log(request.status);
-  }
-});
-request.onerror = () => {
-  console.log('error');
-};
-request.open('GET', 'https://api.twitch.tv/kraken/streams/?client_id=ry76htktupl8mlicd29yf9xhrc2opo&game=League%20of%20Legends&limit=20', true);
+  });
+  request.onerror = () => {
+    console.log('error');
+  };
+  request.open('GET', url, true);
 
-request.send(null);
+  request.send(null);
+}
+loadData('.wrapper', '');
+let parameter = loadData('.wrapper', '');;
+var update = document.querySelector('.updateButton');
+
+window.onload=function(){
+  var update = document.querySelector('.updateButton');
+  update.addEventListener('click',parameter);
+}
+
